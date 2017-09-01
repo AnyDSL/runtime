f7b9e01e220e5aaf892e22037e0cee18b86b32eb
akifoezkan <akif.oezkan@fau.de>
2017-09-01 15:15:05 +0200

    opencl_fpga_accelerator is removed

--- src/opencl_platform.cpp
+++ src/opencl_platform.cpp
@@ -74,7 +74,7 @@ static std::string get_opencl_error_code_str(int error) {
         CL_ERROR_CODE(CL_INVALID_MIP_LEVEL)
         CL_ERROR_CODE(CL_INVALID_GLOBAL_WORK_SIZE)
         #ifdef CL_VERSION_1_1
-        CL_ERROR_CODE(CL_INVALID_PROPERTY)
+//CL_ERROR_CODE(CL_INVALID_PROPERTY)
         #endif
         #ifdef CL_VERSION_1_2
         CL_ERROR_CODE(CL_INVALID_IMAGE_DESCRIPTOR)
