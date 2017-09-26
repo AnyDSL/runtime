ce0f0f88e8a19a96b227641c1ec56bd54fa6d88c
Richard Membarth <richard.membarth@uni-saarland.de>
2017-09-26 15:18:19 +0200

    Patch LLVM 4.0 local_unnamed_addr attributes.

--- post-patcher.py
+++ post-patcher.py
@@ -7,26 +7,39 @@ def patch_llvmir(rttype):
     result = []
     filename = basename+"."+rttype
     if os.path.isfile(filename):
-        print("Patching {0}".format(filename))
         with open(filename) as f:
             for line in f:
                 if rttype=="nvvm":
+                    matched = False
+
                     # patch to opaque identity functions
                     m = re.match('^declare (.*) @(magic_.*_id)\((.*)\)\n$', line)
                     if m is not None:
                         ty1, fname, ty2 = m.groups()
                         assert ty1 == ty2, "Argument and return types of magic IDs must match"
-                        print("Patching magic ID {0}".format(fname))
+                        print("Patching magic ID {0} in {1}".format(fname, filename))
                         # emit definition instead
                         result.append('define {0} @{1}({0} %name) {{\n'.format(ty1, fname))
                         result.append('  ret {0} %name\n'.format(ty1))
                         result.append('}\n')
+                        matched = True
+
+                    # patch unnamed_addr and local_unnamed_addr attributes
+                    m = re.match('^(define .* @.*\(.*\)) (unnamed_addr|local_unnamed_addr)(.*{)\n$', line)
+                    if m is not None:
+                        first, attr, last = m.groups()
+                        print("Patching '{0}' in {1}".format(attr, filename))
+                        result.append('{0}{1}\n'.format(first, last))
+                        matched = True
+
                     # remove source_filename information
-                    elif 'source_filename = ' in line:
-                        print("Removing 'source_filename' information")
+                    if 'source_filename = ' in line:
+                        print("Removing 'source_filename' in {0}".format(filename))
                         result.append(';{0}'.format(line))
+                        matched = True
+
                     # it's a normal line, keep it
-                    else:
+                    if not matched:
                         result.append(line)
         # we have the patched thing, write it
         with open(filename, "w") as f:
@@ -48,7 +61,7 @@ def patch_cfiles(rttype):
                 m = re.match('^(.*) = (magic_.*_id)\((.*)\);\n$', line)
                 if m is not None:
                     lhs, fname, arg = m.groups()
-                    print("Patching magic ID {0}".format(fname))
+                    print("Patching magic ID {0} in {1}".format(fname, filename))
                     # emit definition instead
                     result.append('{0} = {1};\n'.format(lhs, arg))
                 else:
