21d92455701e5bd65d23d5d98eea7c1b0fc5362c
Richard Membarth <richard.membarth@uni-saarland.de>
2017-10-31 23:57:08 +0100

    Cleanup: string(CONCAT ...)

--- cmake/check_amdgpuir.cmake
+++ cmake/check_amdgpuir.cmake
@@ -5,7 +5,13 @@ endif()
 
 # .amdgpu -> .gcn
 if(EXISTS ${_basename}.amdgpu)
-    execute_process(COMMAND echo "define i32 @__oclc_finite_only_opt() { ret i32 0 } define i32 @__oclc_unsafe_math_opt() { ret i32 0 } define i32 @__oclc_daz_opt() { ret i32 0 } define i32 @__oclc_amd_opt() { ret i32 1 } define i32 @__oclc_correctly_rounded_sqrt32() { ret i32 1 } define i32 @__oclc_ISA_version() { ret i32 803 }" OUTPUT_FILE ${_basename}.ocml.ll)
+    string(CONCAT OCML_CONFIG "define i32 @__oclc_finite_only_opt() { ret i32 0 }\n"
+                              "define i32 @__oclc_unsafe_math_opt() { ret i32 0 }\n"
+                              "define i32 @__oclc_daz_opt() { ret i32 0 }\n"
+                              "define i32 @__oclc_amd_opt() { ret i32 1 }\n"
+                              "define i32 @__oclc_correctly_rounded_sqrt32() { ret i32 1 }\n"
+                              "define i32 @__oclc_ISA_version() { ret i32 803 }")
+    execute_process(COMMAND echo "${OCML_CONFIG}" OUTPUT_FILE ${_basename}.ocml.ll)
     execute_process(COMMAND /opt/rocm/hcc/compiler/bin/llvm-link ${_basename}.amdgpu ${_basename}.ocml.ll /opt/rocm/lib/ocml.amdgcn.bc /opt/rocm/lib/irif.amdgcn.bc -o ${_basename}.amdgpu.linked)
     execute_process(COMMAND /opt/rocm/hcc/compiler/bin/llc -mtriple=amdgcn-amd-amdhsa -mcpu=fiji -filetype=obj ${_basename}.amdgpu.linked -o ${_basename}.gcn.rel)
     execute_process(COMMAND /opt/rocm/hcc/compiler/bin/ld.lld -shared ${_basename}.gcn.rel -o ${_basename}.gcn)
