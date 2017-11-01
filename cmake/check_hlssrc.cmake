32cb87e1c48d9cbf60f0a6386a0fa3a37b646e1d
Richard Membarth <richard.membarth@uni-saarland.de>
2017-11-01 14:36:08 +0100

    HLS: support for multiple top level functions.

--- cmake/check_hlssrc.cmake
+++ cmake/check_hlssrc.cmake
@@ -2,19 +2,21 @@
 if(EXISTS ${_basename}.hls)
     execute_process(COMMAND cp ${_basename}.hls ${_basename}_hls.cpp)
     execute_process(COMMAND echo "int main() { return 0; }" OUTPUT_FILE ${_basename}_tb.cpp)
-    execute_process(COMMAND awk -F "[ (]+" "/void .*;/{ print $2 }" ${_basename}.hls
-                    COMMAND tr "\n" " " OUTPUT_VARIABLE top_funs)
-    string(CONCAT TCL_SCRIPT "open_project anydsl_fpga\n"
-                             "set_top ${top_funs}\n"
-                             "add_files ${_basename}_hls.cpp\n"
-                             "add_files -tb ${_basename}_tb.cpp\n"
-                             "open_solution vivado_hls\n"
-                             "set_part {xc7k70tfbv676-2}\n"
-                             "create_clock -period 10.0 -name default\n"
-                             "csim_design -ldflags {-lrt} -clean\n"
-                             "#csynth_design\n"
-                             "#export_design -format ip_catalog -evaluate verilog\n"
-                             "exit")
-    execute_process(COMMAND echo "${TCL_SCRIPT}" OUTPUT_FILE ${_basename}.tcl)
-    execute_process(COMMAND vivado_hls -f ${_basename}.tcl)
+    execute_process(COMMAND awk -F "[ (]+" "/void .*;/{ ORS=\";\"; print $2 }" ${_basename}.hls
+                    COMMAND awk "{ ORS=\"\"; sub(/;$/,\"\"); print }" OUTPUT_VARIABLE kernels)
+    foreach(kernel ${kernels})
+        string(CONCAT tcl_script "open_project anydsl_fpga\n"
+                                 "set_top ${kernel}\n"
+                                 "add_files ${_basename}_hls.cpp\n"
+                                 "add_files -tb ${_basename}_tb.cpp\n"
+                                 "open_solution ${_basename}_${kernel}\n"
+                                 "set_part {xc7k70tfbv676-2}\n"
+                                 "create_clock -period 10.0 -name default\n"
+                                 "csim_design -ldflags {-lrt} -clean\n"
+                                 "#csynth_design\n"
+                                 "#export_design -format ip_catalog -evaluate verilog\n"
+                                 "exit")
+        execute_process(COMMAND echo "${tcl_script}" OUTPUT_FILE ${_basename}_${kernel}.tcl)
+        execute_process(COMMAND vivado_hls -f ${_basename}_${kernel}.tcl)
+    endforeach()
 endif()
