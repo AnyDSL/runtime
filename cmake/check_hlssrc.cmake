0bfebca402c5ecebb7c472986f2cf31b8e830362
Akif Oezkan <akif.oezkan@fau.de>
2017-11-07 21:35:18 +0100

    frequency and part number in hls is changed

--- cmake/check_hlssrc.cmake
+++ cmake/check_hlssrc.cmake
@@ -10,11 +10,11 @@ if(EXISTS ${_basename}.hls)
                                  "add_files ${_basename}_hls.cpp\n"
                                  "add_files -tb ${_basename}_tb.cpp\n"
                                  "open_solution ${_basename}_${kernel}\n"
-                                 "set_part {xc7k70tfbv676-2}\n"
-                                 "create_clock -period 10.0 -name default\n"
+                                 "set_part {xc7z100ffg900-2}\n"
+                                 "create_clock -period 6.5 -name default\n"
                                  "csim_design -compiler clang -ldflags {-lrt} -clean\n"
-                                 "#csynth_design\n"
-                                 "#export_design -format ip_catalog -evaluate verilog\n"
+                                 "csynth_design\n"
+                                 "export_design -format ip_catalog -evaluate verilog\n"
                                  "exit")
         execute_process(COMMAND echo "${tcl_script}" OUTPUT_FILE ${_basename}_${kernel}.tcl)
         execute_process(COMMAND vivado_hls -f ${_basename}_${kernel}.tcl)
