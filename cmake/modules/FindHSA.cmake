6c526da78d37acef7778417d88eacf3e4ac315ad
Richard Membarth <richard.membarth@uni-saarland.de>
2018-09-15 23:40:07 +0200

    Whitespace.

--- cmake/modules/FindHSA.cmake
+++ cmake/modules/FindHSA.cmake
@@ -1,12 +1,12 @@
 # Find the HSA includes and library
 #
 # To set manually the paths, define these environment variables:
-#  HSA_DIR              - Include path
+#  HSA_DIR          - Include path
 #
 # Once done this will define
-#  HSA_INCLUDE_DIRS  - where to find HSA include files
-#  HSA_LIBRARIES     - where to find HSA libs
-#  HSA_FOUND         - True if HSA is found
+#  HSA_INCLUDE_DIRS - where to find HSA include files
+#  HSA_LIBRARIES    - where to find HSA libs
+#  HSA_FOUND        - True if HSA is found
 
 find_path(HSA_INCLUDE_DIR hsa.h         PATHS ${HSA_DIR} PATH_SUFFIXES include/hsa)
 find_library(HSA_LIBRARY  hsa-runtime64 PATHS ${HSA_DIR} PATH_SUFFIXES lib)
