52995b6e00715757d92ea838cca9d9cd27859fda
Stefan Lemme <stefan.lemme@dfki.de>
2021-04-04 16:04:50 +0200

    Rework symbol visibility to fix linker errors on windows

--- src/anydsl_jit.h
+++ src/anydsl_jit.h
@@ -10,9 +10,9 @@ class Runtime;
 AnyDSL_runtime_API Runtime& runtime();
 
 #ifdef AnyDSL_runtime_HAS_JIT_SUPPORT
-AnyDSL_runtime_API void anydsl_link(const char*);
-AnyDSL_runtime_API int32_t anydsl_compile(const char*, uint32_t, uint32_t);
-AnyDSL_runtime_API void *anydsl_lookup_function(int32_t, const char*);
+AnyDSL_runtime_jit_API void anydsl_link(const char*);
+AnyDSL_runtime_jit_API int32_t anydsl_compile(const char*, uint32_t, uint32_t);
+AnyDSL_runtime_jit_API void *anydsl_lookup_function(int32_t, const char*);
 #endif
 
 #endif
