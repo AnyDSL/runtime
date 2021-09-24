efa2cfb537b30f795eb0555a465cac6171879bf1
Richard Membarth <richard.membarth@uni-saarland.de>
2021-09-24 23:49:48 +0200

    Compile fix.

--- src/opencl_platform.cpp
+++ src/opencl_platform.cpp
@@ -392,7 +392,7 @@ void OpenCLPlatform::launch_kernel(DeviceId dev, const LaunchParams& launch_para
     if (devices_[dev].is_intel_fpga || devices_[dev].is_xilinx_fpga)
         queue = devices_[dev].kernels_queue[kernel];
 
-    if (devices_[dev].is_xilinx_fpga && block[1] == 1 && block[2] == 1 && block[3] == 1) {
+    if (devices_[dev].is_xilinx_fpga && global_work_size[1] == 1 && global_work_size[2] == 1 && global_work_size[3] == 1) {
         cl_int err = clEnqueueTask(queue, kernel, 0, NULL, &event);
         CHECK_OPENCL(err, "clEnqueueTask()");
     } else {
