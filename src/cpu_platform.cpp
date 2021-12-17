93667d080811483047d7106922b5765db21e4feb
Michael Kenzel <michael.kenzel@gmail.com>
2021-12-17 11:47:14 +0100

    Simpler implementation of CpuPlatform device name for Win32

--- src/cpu_platform.cpp
+++ src/cpu_platform.cpp
@@ -1,6 +1,7 @@
 #include "cpu_platform.h"
 #include "runtime.h"
 
+#include <cstddef>
 #include <algorithm>
 #include <fstream>
 #include <iterator>
@@ -10,96 +11,11 @@
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #elif defined(_WIN32)
-#include <new>
 #include <memory>
 
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
-#include <objbase.h>
-
-#include <WbemIdl.h>
-
-namespace
-{
-    struct COM_deleter
-    {
-        void operator ()(IUnknown* ptr) const
-        {
-            ptr->Release();
-        }
-    };
-
-    template <typename T>
-    using unique_com_ptr = std::unique_ptr<T, COM_deleter>;
-
-    auto make_bstr(const wchar_t* str)
-    {
-        struct deleter
-        {
-            void operator ()(BSTR ptr) const
-            {
-                SysFreeString(ptr);
-            }
-        };
-
-        return std::unique_ptr<wchar_t[], deleter> { SysAllocString(str) };
-    }
-
-    auto bstr_length(BSTR str)
-    {
-        return *std::launder(reinterpret_cast<const DWORD*>(reinterpret_cast<const std::byte*>(str) - sizeof(DWORD))) / 2;
-    }
-
-    std::string utf16_to_utf8(const WCHAR* input, int input_length)
-    {
-        int output_length = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, input, input_length, nullptr, 0, nullptr, nullptr);
-
-        if (output_length <= 0)
-            error("failed to get UTF-8 string length");
-
-        std::string output(output_length, '\0');
-
-        if (WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, input, input_length, &output[0], output_length, nullptr, nullptr) <= 0)
-            error("failed to get UTF-8 string");
-
-        return output;
-    }
-
-    auto create_WbemLocator()
-    {
-        void* locator;
-        if (FAILED(CoCreateInstance(CLSID_WbemLocator, nullptr, CLSCTX_INPROC_SERVER, IID_IWbemLocator, &locator)))
-            error("failed to create WbemLocator");
-        return unique_com_ptr<IWbemLocator>(static_cast<IWbemLocator*>(locator));
-    }
-
-    auto connect_server(IWbemLocator* locator, const wchar_t* server)
-    {
-        IWbemServices* services;
-        if (FAILED(locator->ConnectServer(make_bstr(server).get(), nullptr, nullptr, nullptr, 0L, nullptr, nullptr, &services)))
-            error("failed to connect to WBEM resource");
-        return unique_com_ptr<IWbemServices>(services);
-    }
-
-    auto connect_WMI()
-    {
-        auto services = connect_server(create_WbemLocator().get(), L"root\\cimv2");
-
-        if (FAILED(CoSetProxyBlanket(services.get(), RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, nullptr, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE)))
-            error("CoSetProxyBlanket failed");
-
-        return services;
-    }
-
-    auto exec_query(IWbemServices* services, const wchar_t* query)
-    {
-        IEnumWbemClassObject* enumerator;
-        if (FAILED(services->ExecQuery(make_bstr(L"WQL").get(), make_bstr(query).get(), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, nullptr, &enumerator)))
-            error("failed to execute WMI query");
-        return unique_com_ptr<IEnumWbemClassObject>(enumerator);
-    }
-}
 #endif
 
 CpuPlatform::CpuPlatform(Runtime* runtime)
@@ -108,34 +24,38 @@ CpuPlatform::CpuPlatform(Runtime* runtime)
     #if defined(__APPLE__)
     size_t buf_len;
     sysctlbyname("machdep.cpu.brand_string", nullptr, &buf_len, nullptr, 0);
-    std::string buf(buf_len, ' ');
-    sysctlbyname("machdep.cpu.brand_string", (void*)buf.c_str(), &buf_len, nullptr, 0);
-    device_name_ = buf;
+    device_name_.resize(buf_len, '\0');
+    sysctlbyname("machdep.cpu.brand_string", &device_name_[0], &buf_len, nullptr, 0);
     #elif defined(_WIN32)
-    if (FAILED(CoInitializeEx(nullptr, COINIT_MULTITHREADED)))
-        error("CoInitializeEx failed");
+    HKEY key;
+    if (RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0U, KEY_QUERY_VALUE, &key) != ERROR_SUCCESS)
+        error("failed to open processor information registry key");
 
-    if (FAILED(CoInitializeSecurity(nullptr, -1L, nullptr, nullptr, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE, nullptr)))
-        error("CoInitializeSecurity failed");
+    DWORD cpu_name_type;
+    DWORD cpu_name_size;
+    if (RegQueryValueExW(key, L"ProcessorNameString", nullptr, &cpu_name_type, nullptr, &cpu_name_size) != ERROR_SUCCESS)
+        error("failed to query processor name string length");
 
-    auto services = connect_WMI();
+    if (cpu_name_type != REG_SZ)
+        error("unexpected type for processor name string");
 
-    auto query_result = exec_query(services.get(), L"SELECT * FROM Win32_Processor");
+    int cpu_name_length = cpu_name_size / sizeof(wchar_t);
 
-    IWbemClassObject* obj;
-    ULONG returned;
-    if (FAILED(query_result->Next(WBEM_INFINITE, 1, &obj, &returned)) || returned != 1)
-        error("failed to get WMI query result");
+    auto buffer = std::unique_ptr<wchar_t[]> { new wchar_t[cpu_name_length] };
+    if (RegQueryValueExW(key, L"ProcessorNameString", nullptr, &cpu_name_type, reinterpret_cast<LPBYTE>(&buffer[0]), &cpu_name_size) != ERROR_SUCCESS)
+        error("failed to query processor name string");
 
-    VARIANT value;
-    CIMTYPE type;
-    if (FAILED(obj->Get(L"Name", 0L, &value, &type, nullptr)))
-        error("failed to get processor name");
+    RegCloseKey(key);
 
-    if (type != CIM_STRING)
-        error("unexpected value type for processor name");
+    int u8_cpu_name_length = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, &buffer[0], cpu_name_length, nullptr, 0, nullptr, nullptr);
 
-    device_name_ = utf16_to_utf8(value.bstrVal, bstr_length(value.bstrVal));
+    if (u8_cpu_name_length <= 0)
+        error("failed to compute converted UTF-8 CPU name string length");
+
+    device_name_.resize(u8_cpu_name_length, '\0');
+
+    if (WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, &buffer[0], cpu_name_length, &device_name_[0], u8_cpu_name_length, nullptr, nullptr) <= 0)
+        error("failed to convert CPU name string to UTF-8");
     #else
     std::ifstream cpuinfo("/proc/cpuinfo");
 
