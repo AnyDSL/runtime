bbcb1795eb4f842cff9643e5e3cdabb832386bc2
pooyaww <pooya.w@gmail.com>
2024-08-03 21:19:17 +0200

    AI Engine build module

--- cmake/modules/FindXHLS.cmake
+++ cmake/modules/FindXHLS.cmake
@@ -7,6 +7,9 @@ find_program(Xilinx_VPP v++ PATHS ${VITIS_ROOT_DIR}/bin NO_DEFAULT_PATH)
 find_program(Xilinx_PLATFORM_INFO platforminfo PATH ${VITIS_ROOT_DIR}/bin NO_DEFAULT_PATH)
 find_program(Xilinx_KERNEL_INFO kernelinfo PATH ${VITIS_ROOT_DIR}/bin NO_DEFAULT_PATH)
 find_program(Xilinx_EMU_CONFIG emconfigutil PATH ${VITIS_ROOT_DIR}/bin NO_DEFAULT_PATH)
+find_program(Xilinx_AIE_COMPILER aiecompiler PATH ${VITIS_ROOT_DIR}/aietools/bin NO_DEFAULT_PATH)
+find_program(Xilinx_AIE_SIMULATOR aiesimulator PATH ${VITIS_ROOT_DIR}/aietools/bin NO_DEFAULT_PATH)
+find_program(Xilinx_AIE_X86SIMULATOR x86simulator PATH ${VITIS_ROOT_DIR}/aietools/bin NO_DEFAULT_PATH)
 
 
 get_filename_component(VITIS_VERSION "${VITIS_ROOT_DIR}" NAME)
@@ -28,6 +31,10 @@ find_path(Xilinx_OPENCL_EXTENSIONS_INCLUDE_DIR cl_ext.h PATHS ${XILINX_RUNTIME_D
 get_filename_component(Xilinx_OPENCL_EXTENSIONS_INCLUDE_DIR ${Xilinx_OPENCL_EXTENSIONS_INCLUDE_DIR} DIRECTORY)
 set(Xilinx_INCLUDE_DIRS ${Xilinx_HLS_INCLUDE_DIR} ${Xilinx_OPENCL_EXTENSIONS_INCLUDE_DIR})
 
+
+find_path(Xilinx_AIE_INCLUDE_DIR adf.h PATHS ${VITIS_ROOT_DIR}/aietools/include NO_DEFAULT_PATH)
+set(Xilinx_AIE_INCLUDE_DIRS ${Xilinx_AIE_INCLUDE_DIRS})
+
 mark_as_advanced(
     XILINX_RUNTIME_DIR
     XRT_SEARCH_PATH
@@ -39,6 +46,10 @@ mark_as_advanced(
     Xilinx_PLATFORM_INFO
     Xilinx_KERNEL_INFO
     Xilinx_EMU_CONFIG
+    Xilinx_AIE_COMPILER
+    Xilinx_AIE_SIMULATOR
+    Xilinx_AIE_X86SIMULATOR
+    Xilinx_AIE_INCLUDE_DIRS
     Xilinx_LIBRARIES
     Xilinx_INCLUDE_DIRS)
 
@@ -51,4 +62,11 @@ find_package_handle_standard_args(XHLS DEFAULT_MSG
     Xilinx_PLATFORM_INFO
     Xilinx_KERNEL_INFO
     Xilinx_EMU_CONFIG
+)
+
+find_package_handle_standard_args(AIE DEFAULT_MSG
+    Xilinx_AIE_COMPILER
+    Xilinx_AIE_SIMULATOR
+    Xilinx_AIE_X86SIMULATOR
+    Xilinx_AIE_INCLUDE_DIRS
 )
